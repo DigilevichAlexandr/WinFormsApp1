22ca3199dca2dc1f994af9ba06a3c86a7c02a5d7
alexandr digilevich <digilevich.alexandr@gmail.com>
2021-11-17 22:55:58 +0200

    Add project files.

---
+++ BLL/Services/ServiceCars.cs
@@ -0,0 +1,24 @@
+﻿using BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class ServiceCars : IServiceCars
+    {
+        public ServiceCars(
+            //IDataContext dataContext
+            )
+        {
+
+        }
+
+        public IEnumerable<Car> GetSomeThing()
+        {
+            return null;
+        }
+    }
+}
